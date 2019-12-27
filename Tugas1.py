4c5ff4fd0f29d180d2850b9eac978517be6a87a6
Hankurs12 <kurniahana9@gmail.com>
2019-12-27 11:41:30 +0700

    tugas pertama

---
+++ Tugas1.py
@@ -0,0 +1,12 @@
+a =12
+b =10
+c =11
+d = a + b + c
+e = a - b - c
+f = a * b * c
+g = a // b // c
+
+print(" %d + %d + %d = "%(a,b,c),d)
+print(" %d - %d - %d = "%(a,b,c),e)
+print(" %d x %d x %d = "%(a,b,c),f)
+print(" %d \\ %d \\ %d = "%(a,b,c),g)
